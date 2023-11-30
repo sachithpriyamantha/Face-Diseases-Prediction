1adf4af1cacf1c87351e657c7085b7b150b99486
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-11-30 22:25:02 +0530

    upd

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 import cv2
-from keras.models import model_from_json
+from keras.models import load_model
 import numpy as np
-# from keras_preprocessing.image import load_img
-json_file = open("faceskindiseases.json", "r")
-model_json = json_file.read()
-json_file.close()
-model = model_from_json(model_json)
+import matplotlib.pyplot as plt
+from math import sqrt
 
-model.load_weights("my_model.h5")
-haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-face_cascade=cv2.CascadeClassifier(haar_file)
 
-def extract_features(image):
-    feature = np.array(image)
-    feature = feature.reshape(1,48,48,1)
-    return feature/255.0
 
-webcam=cv2.VideoCapture(0)
-labels = {0 : 'Acne', 1 : 'Actinic Keratosis', 2 : 'Basal Cell Carcinoma', 3 : 'Eczemaa', 4 : 'Rosacea', }
+# Load the pre-trained model
+model = load_model('my_model.h5')
+
+# Haar Cascade for face detection
+haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(haar_file)
+
+# Function to preprocess the image
+def preprocess_image(image):
+    processed_image = cv2.resize(image, (48, 48))
+    processed_image = np.array(processed_image).reshape(1, 48, 48, 1)
+    return processed_image / 255.0
+
+# Labels for the diseases
+labels = {0: 'Acne', 1: 'Actinic Keratosis', 2: 'Basal Cell Carcinoma', 3: 'Eczema', 4: 'Rosacea'}
+
+# Starting the webcam
+webcam = cv2.VideoCapture(0)
+
 while True:
-    i,im=webcam.read()
-    gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    faces=face_cascade.detectMultiScale(im,1.3,5)
-    try: 
-        for (p,q,r,s) in faces:
-            image = gray[q:q+s,p:p+r]
-            cv2.rectangle(im,(p,q),(p+r,q+s),(255,0,0),2)
-            image = cv2.resize(image,(48,48))
-            img = extract_features(image)
-            pred = model.predict(img)
-            prediction_label = labels[pred.argmax()]
-            # print("Predicted Output:", prediction_label)
-            # cv2.putText(im,prediction_label)
-            cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
-        cv2.imshow("Output",im)
-        cv2.waitKey(27)
-    except cv2.error:
-        pass
+    ret, frame = webcam.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        face_img = gray[y:y+h, x:x+w]
+        preprocessed = preprocess_image(face_img)
+
+        # Predicting the disease
+        prediction = model.predict(preprocessed)
+        disease_label = labels[np.argmax(prediction)]
+
+        # Drawing a rectangle and putting text
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.putText(frame, disease_label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+
+    cv2.imshow('Skin Disease Detection', frame)
+
+    if cv2.waitKey(27) & 0xFF == ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
