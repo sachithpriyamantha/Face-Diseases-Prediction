4654ad49a384ff4a140391faab13dc0f306f4d10
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-03-26 15:18:12 +0530

    Update

--- App/face_diseases_app/lib/model/user.dart
+++ App/face_diseases_app/lib/model/user.dart
@@ -1,8 +1,13 @@
 class User {
   final String name;
   final String username;
+  //final String contact;
   final String image;
+  final String info;
   bool isFollowedByMe;
+  
 
-  User(this.name, this.username, this.image, this.isFollowedByMe);
+  User(this.name, this.username,  this.image,this.info,this.isFollowedByMe);
+
+  get followersCount => null;
 }
