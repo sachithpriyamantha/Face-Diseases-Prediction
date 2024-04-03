021433814bb1cf7ade592ddac49cfd69b858b730
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-04-03 08:21:51 +0530

    Updateed

--- App/face_diseases_app/lib/Pages/chat.dart
+++ App/face_diseases_app/lib/Pages/chat.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+/*import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -99,5 +99,141 @@ class ChatScreen extends StatelessWidget {
       });
       _messageController.clear();
     }
+  }
+}*/
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  final TextEditingController _messageController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Group Chat'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('messages').orderBy('timestamp', descending: true).snapshots(),
+              builder: (ctx, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                final messages = snapshot.data!.docs;
+
+                return ListView.builder(
+                  reverse: true,
+                  itemCount: messages.length,
+                  itemBuilder: (ctx, index) {
+                    final messageData = messages[index].data() as Map<String, dynamic>;
+                    return MessageBubble(
+                      message: messageData['message'],
+                      isMe: user!.uid == messageData['senderId'],
+                      key: ValueKey(messages[index].id),
+                      username: messageData['senderName'],
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(labelText: 'Send a message...'),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () => _sendMessage(user),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _sendMessage(User? user) {
+    final messageText = _messageController.text.trim();
+    if (messageText.isNotEmpty && user != null) {
+      FirebaseFirestore.instance.collection('messages').add({
+        'message': messageText,
+        'senderId': user.uid,
+        'senderName': user.email, // Consider using a more user-friendly name, if available
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+      _messageController.clear();
+    }
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({
+    Key? key,
+    required this.message,
+    required this.isMe,
+    required this.username,
+  }) : super(key: key);
+
+  final String message;
+  final bool isMe;
+  final String username;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: isMe ? Colors.grey[300] : Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+              bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+            ),
+          ),
+          width: 140,
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          child: Column(
+            crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                username,
+                style: TextStyle(fontWeight: FontWeight.bold, color: isMe ? Colors.black : Colors.white),
+              ),
+              Text(
+                message,
+                style: TextStyle(color: isMe ? Colors.black : Colors.white),
+                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
