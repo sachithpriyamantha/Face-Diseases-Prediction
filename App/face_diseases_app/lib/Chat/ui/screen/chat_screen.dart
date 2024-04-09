5c83ba38998d1297fe73f3ea032ba3562188dffd
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-04-09 11:38:06 +0530

    Update

--- App/face_diseases_app/lib/Chat/ui/screen/chat_screen.dart
+++ App/face_diseases_app/lib/Chat/ui/screen/chat_screen.dart
@@ -308,7 +308,7 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:face_diseases_app/Chat/ui/widgets/message.dart'; // Ensure this path is correct
-import 'package:face_diseases_app/Pages/dashboard.dart'; // Ensure this path is correct
+import 'package:face_diseases_app/Pages/dashboard.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -328,70 +328,87 @@ class Chat extends StatefulWidget {
 class _ChatState extends State<Chat> {
   final CollectionReference _messageCollectionRef =
       FirebaseFirestore.instance.collection('messages');
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  //final FirebaseAuth _auth = FirebaseAuth.instance;
   final TextEditingController messageController = TextEditingController();
   final ScrollController scrollController = ScrollController();
+  
+
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text('Menu'),
-            ),
-            ListTile(
-              title: Text('Log out'),
-              onTap: () async {
-                await _auth.signOut();
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => Dashboard()));
-              },
-            ),
-          ],
-        ),
-      ),
+
       appBar: AppBar(
-        title: Text("Group Chat"),
-      ),
+  leading: IconButton(
+    icon: Icon(Icons.arrow_back, color: Color.fromARGB(255, 250, 250, 250)),
+    onPressed: () => Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => Dashboard()),
+    ),
+  ),
+  title: Text(
+    'Group Chat',
+    style: TextStyle(color: Colors.white),
+  ),
+  backgroundColor: Color.fromARGB(255, 22, 0, 147),
+),
+
+
+
+
+
+      /***************************************Body****************************************************************** */
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: _messageCollectionRef.orderBy('date').snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData)
-                    return Center(child: CircularProgressIndicator());
+        
+Expanded(
+  child: StreamBuilder<QuerySnapshot>(
+    stream: _messageCollectionRef.orderBy('date').snapshots(),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
 
-                  List<DocumentSnapshot> docs = snapshot.data!.docs;
-                  List<Widget> messages = docs.map((doc) {
-                    Map<String, dynamic>? data =
-                        doc.data() as Map<String, dynamic>?;
-                    if (data != null) {
-                      return Message(
-                        from: data['from'] ?? 'Unknown',
-                        text: data['text'] ?? '',
-                        imageUrl: data[
-                            'imageUrl'], // Assuming Message widget can handle nullable imageUrl
-                        me: widget.user.email == data['from'],
-                      );
-                    } else {
-                      return SizedBox.shrink();
-                    }
-                  }).toList();
+      // Ensuring docs is never null
+      List<DocumentSnapshot> docs = snapshot.data?.docs ?? [];
 
-                  return ListView(
-                    controller: scrollController,
-                    children: messages,
-                  );
-                },
-              ),
-            ),
+      // Initializing messages as an empty list to ensure it's not null
+      List<Widget> messages = [];
+
+      // Safely iterate over docs and build your widgets
+      if (docs.isNotEmpty) {
+        messages = docs.map((doc) {
+          Map<String, dynamic>? data = doc.data() as Map<String, dynamic>?;
+          // Ensure data is not null before proceeding
+          if (data != null) {
+            return Message(
+              from: data['from'] ?? 'Unknown',
+              text: data['text'] ?? '',
+              imageUrl: data['imageUrl'], // Assuming Message widget can handle nullable imageUrl
+              me: widget.user.email == data['from'],
+              status: data['status'] ?? 'sent', // Handling status
+            );
+          } else {
+            // Returning a placeholder or empty widget if data is null
+            return SizedBox.shrink();
+          }
+        }).toList();
+      }
+
+      return ListView(
+        controller: scrollController,
+        children: messages,
+      );
+    },
+  ),
+),
+
+
+
+
+                
+
             _buildInput(),
           ],
         ),
@@ -456,16 +473,13 @@ class _ChatState extends State<Chat> {
 
   Future<void> _sendMessage({String? text, String? imageUrl}) async {
     final messageData = {
-      'from': widget.user.email,
-      'date': DateTime.now().toIso8601String(),
+    'from': widget.user.email,
+    'date': DateTime.now().toIso8601String(),
+    'status': 'sent', // Initial status of the message
+    // Add text and imageUrl if they are not null
+    if (text?.isNotEmpty ?? false) 'text': text,
+    if (imageUrl != null) 'imageUrl': imageUrl,
     };
-
-    if (text?.isNotEmpty ?? false) {
-      messageData['text'] = text;
-    }
-    if (imageUrl != null) {
-      messageData['imageUrl'] = imageUrl;
-    }
 
     await _messageCollectionRef.add(messageData);
     messageController.clear();
--- App/face_diseases_app/lib/Chat/ui/widgets/message.dart
+++ App/face_diseases_app/lib/Chat/ui/widgets/message.dart
@@ -5,6 +5,7 @@ class Message extends StatelessWidget {
   final String? text;
   final String? imageUrl;
   final bool me;
+  final String? status;
 
   const Message({
     Key? key,
@@ -12,25 +13,50 @@ class Message extends StatelessWidget {
     this.text,
     this.imageUrl,
     required this.me,
+    this.status,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: me ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-      children: [
-        Text(from, style: TextStyle(fontWeight: FontWeight.bold)),
-        if (text != null && text!.isNotEmpty) 
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: Text(text!),
-          ),
-        if (imageUrl != null)
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: Image.network(imageUrl!, width: 200, fit: BoxFit.cover), // Adjust size as needed
-          ),
-      ],
+    // Set different background colors for sent and received messages
+    Color backgroundColor = me ? Colors.lightBlueAccent : Colors.grey[300]!;
+    Color textColor = me ? Colors.white : Colors.black;
+
+    return Align(
+      // Align messages to the right if sent by the user, otherwise to the left
+      alignment: me ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(from, style: TextStyle(fontWeight: FontWeight.bold, color: textColor)),
+            if (text != null && text!.trim().isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Text(text!, style: TextStyle(color: textColor)),
+              ),
+            if (imageUrl != null)
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Image.network(imageUrl!, width: 200, height: 200, fit: BoxFit.cover),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
