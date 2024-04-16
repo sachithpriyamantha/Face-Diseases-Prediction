7ea40745a221d5dd5a52834d11cd9ccd6953dd64
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-04-16 14:39:20 +0530

    Update

--- App/face_diseases_app/lib/Pages/channel.dart
+++ App/face_diseases_app/lib/Pages/channel.dart
@@ -1,4 +1,4 @@
-import 'package:face_diseases_app/Pages/dashboard.dart';
+/*import 'package:face_diseases_app/Pages/dashboard.dart';
 import 'package:face_diseases_app/model/user.dart';
 import 'package:flutter/material.dart';
 
@@ -338,5 +338,181 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+}
+*/
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:face_diseases_app/Pages/dashboard.dart';
+import 'package:face_diseases_app/model/user.dart';
+import 'package:flutter/material.dart';
+
+class ChannelPage extends StatelessWidget {
+  const ChannelPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final FirestoreService _firestoreService = FirestoreService();
+  List<User> _users = [];
+  List<User> _filteredUsers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _firestoreService.getUsers().listen((userList) {
+      setState(() {
+        _users = userList;
+        _filteredUsers = _users;
+      });
+    });
+  }
+
+  void onSearch(String search) {
+    setState(() {
+      _filteredUsers = _users.where((user) =>
+        user.username.toLowerCase().contains(search.toLowerCase()) ||
+        user.name.toLowerCase().contains(search.toLowerCase())).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Color.fromARGB(255, 250, 250, 250)),
+          onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Dashboard())),
+        ),
+        elevation: 0,
+        backgroundColor: Color.fromARGB(255, 22, 0, 147),
+        title: Container(
+          height: 38,
+          child: TextField(
+            onChanged: onSearch,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Color.fromARGB(255, 255, 255, 255),
+              contentPadding: EdgeInsets.all(0),
+              prefixIcon: Icon(Icons.search, color: Colors.grey.shade500),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+                borderSide: BorderSide.none,
+              ),
+              hintStyle: TextStyle(fontSize: 14, color: Colors.grey.shade500),
+              hintText: "Search Doctor Name or District"
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color.fromARGB(255, 241, 169, 169),
+              Color.fromARGB(255, 157, 235, 179),
+              Color.fromARGB(255, 241, 169, 169),
+            ],
+          ),
+        ),
+        child: ListView.builder(
+          itemCount: _filteredUsers.length,
+          itemBuilder: (context, index) => userComponent(user: _filteredUsers[index]),
+        ),
+      ),
+    );
+  }
+
+  Widget userComponent({required User user}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(children: [
+            Container(
+              width: 60,
+              height: 60,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(user.image, fit: BoxFit.cover),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(user.name, style: const TextStyle(color: Colors.black, fontWeight: FontWeight.w500)),
+                const SizedBox(height: 5),
+                Text(user.username, style: const TextStyle(color: Color.fromARGB(255, 81, 80, 80))),
+              ],
+            ),
+          ]),
+          GestureDetector(
+            onTap: () => showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text(user.name),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Hospital: ${user.username}"),
+                    Text("Information: ${user.info}"),
+                  ],
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text('Close'),
+                  ),
+                ],
+              ),
+            ),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds:300),
+              height: 35,
+              width: 110,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: Colors.transparent),
+              ),
+              child: const Center(
+                child: Text('Info', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FirestoreService {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Stream<List<User>> getUsers() {
+    return firestore.collection('users').snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) => User.fromFirestore(doc.data() as Map<String, dynamic>, doc.id)).toList();
+    });
   }
 }
--- App/face_diseases_app/lib/model/user.dart
+++ App/face_diseases_app/lib/model/user.dart
@@ -1,4 +1,4 @@
-class User {
+/*class User {
   final String name;
   final String username;
   //final String contact;
@@ -10,4 +10,45 @@ class User {
   User(this.name, this.username,  this.image,this.info,this.isFollowedByMe);
 
   get followersCount => null;
+}*/
+
+
+
+class User {
+  final String id;
+  final String name;
+  final String username; // This could be the hospital name or similar
+  final String image;
+  final String info;
+  final bool isFollowedByMe;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.image,
+    required this.info,
+    required this.isFollowedByMe,
+  });
+
+  factory User.fromFirestore(Map<String, dynamic> firestore, String id) {
+    return User(
+      id: id,
+      name: firestore['name'] ?? 'No Name Provided',
+      username: firestore['username'] ?? 'No Username',
+      image: firestore['imageUrl'] ?? 'default_image.jpg',
+      info: firestore['info'] ?? 'No Information Available',
+      isFollowedByMe: firestore['isFollowed'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'username': username,
+      'imageUrl': image,
+      'info': info,
+      'isFollowed': isFollowedByMe,
+    };
+  }
 }
