9fbc62919da315d8806b7a488449cfb253498426
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2024-04-19 01:17:50 +0530

    Update

--- App/face_diseases_app/lib/Pages/dashboard.dart
+++ App/face_diseases_app/lib/Pages/dashboard.dart
@@ -1,4 +1,4 @@
-
+/*
 import 'package:face_diseases_app/Chat/ui/screen/chat_screen.dart';
 import 'package:face_diseases_app/Login/screens/home/ui/home_sceren.dart';
 import 'package:face_diseases_app/Pages/Report.dart';
@@ -333,6 +333,247 @@ class Dashboard extends StatelessWidget {
 
   void setState(Null Function() param0) {}
 }
+*/
 
 
+
+
+
+import 'package:face_diseases_app/Chat/ui/screen/chat_screen.dart';
+import 'package:face_diseases_app/Login/screens/home/ui/home_sceren.dart';
+import 'package:face_diseases_app/Pages/Report.dart';
+import 'package:face_diseases_app/Pages/about.dart';
+import 'package:face_diseases_app/Pages/channel.dart';
+import 'package:face_diseases_app/Pages/face_diseases.dart';
+import 'package:face_diseases_app/Pages/nearbyclinic.dart';
+import 'package:face_diseases_app/Pages/scan.dart';
+import 'package:face_diseases_app/Pages/video.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Dashboard extends StatelessWidget {
+  int index = 2;
+  double? height, width;
+
+  // List of images and titles for dashboard cards
+  List<String> imgData = [
+    "image/Scan.png",
+    "image/report.png",
+    "image/diseases.png",
+    "image/clinic.png",
+    "image/doctor.png",
+    "image/learn.png",
+    "image/chat.png",
+    "image/about.png",
+    "image/profile.png",
+  ];
+
+  List<String> titles = [
+    "Scan",
+    "Daily Report",
+    "Face Diseases",
+    "Near By Clinic",
+    "Doctors",
+    "Learn Videos",
+    "Chat",
+    "About",
+    "Profile",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          buildStaticHeader(context),
+          Expanded(child: buildScrollableContent(context)),
+        ],
+      ),
+    );
+  }
+
+  Widget buildStaticHeader(BuildContext context) {
+    return Container(
+      //color: Color.fromARGB(255, 0, 0, 0),
+      height: height! * 0.25,
+      width: width!,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 35, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: const Icon(Icons.sort, color: Colors.white, size: 40),
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: FirebaseAuth.instance.currentUser!.photoURL == null
+                        ? Image.asset('image/profile.png')
+                        : FadeInImage.assetNetwork(
+                            placeholder: 'image/loading.gif',
+                            image: FirebaseAuth.instance.currentUser!.photoURL!,
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "How can help you Today?",
+                  style: const TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 1,
+                  ),
+                ),
+                const SizedBox(height: 1),
+                const Text(
+                  "Active now",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 136, 135, 135),
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildScrollableContent(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Color.fromARGB(255, 22, 0, 147), Color.fromARGB(255, 0, 0, 0), Color.fromARGB(255, 22, 0, 147)],
+          ),
+          boxShadow: const [BoxShadow(color: Colors.white, spreadRadius: 1, blurRadius: 6)],
+          borderRadius: const BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+        ),
+        width: width,
+        padding: const EdgeInsets.only(bottom: 20),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1.1,
+            mainAxisSpacing: 25,
+          ),
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: imgData.length,
+          itemBuilder: (context, index) => buildDashboardItem(context, index),
+        ),
+      ),
+    );
+  }
+
+  Widget buildDashboardItem(BuildContext context, int index) {
+    return InkWell(
+      onTap: () => handleCardTap(context, index),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Color.fromARGB(255, 0, 0, 0),
+          boxShadow: const [BoxShadow(color: Colors.white, spreadRadius: 1, blurRadius: 6)],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(imgData[index], width: 100),
+            Text(
+              titles[index],
+              style: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void handleCardTap(BuildContext context, int index) {
+    // Navigate to different screens based on the tapped title
+    switch (titles[index]) {
+      case "Scan":
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => ScanPage()));
+        break;
+      case "Daily Report":
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => DailyReportPage()));
+        break;
+      case "Face Diseases":
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => FaceDiseasesPage()));
+        break;
+      case "Near By Clinic":
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => NearByClinicPage()));
+        break;
+      case "Learn Videos":
+        var youtubeVideos;
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        YoutubeVideoListScreen(
+                                            videos: youtubeVideos)));
+        break;
+      case "Profile":
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeScreen()));
+        break;
+      case "Chat":
+        final user = FirebaseAuth.instance.currentUser;
+        if (user != null) {
+          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => Chat(user: user, key: UniqueKey())));
+        }
+        break;
+      case "About":
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => AboutPage()));
+        break;
+      case "Doctors":
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => ChannelPage()));
+        break;
+      default:
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Item Details"),
+              content: Text("Details about the item tapped."),
+              actions: [
+                TextButton(
+                  child: Text("Close"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+        break;
+    }
+  }
+}
 
